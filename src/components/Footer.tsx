aa1eb000433ecab1277bf33178d3b381d9502c13
hankyungsu <hks713@naver.com>
2023-04-29 15:59:14 +0900

    modify

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <p className=' bg-slate-900 text-white py-2 text-sm text-center'>
-      {'hks713의 블로그 입니다.'}
+      {'hks713의 블로그 입니다. | All Right Reserved.'}
     </p>
   );
 }
